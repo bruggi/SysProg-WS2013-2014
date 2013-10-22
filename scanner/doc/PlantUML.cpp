8847a5faf95eb35ca1c8231d8ef2ab5c137ebee4
JohannesKolb <johannes.kolb88@googlemail.com>
2013-10-22 17:40:54 +0200

    added PlantUML.cpp to scanner. It contains plantuml code to draw UMLs.

---
+++ scanner/doc/PlantUML.cpp
@@ -0,0 +1,53 @@
+/*
+ * PlantUML.cpp
+ *
+ *  Created on: 22.10.2013
+ *      Author: typical
+ *
+ *   This file is only for PLANTUML code!
+ */
+
+///@startuml
+
+/// class FSM {
+///   row:int
+///   column:int
+///   currentState:state
+///   currentChar:char
+/// ---
+///   validateChar(char):status
+/// }
+
+/// class Buffer {
+///
+/// ---
+///   getchar():char
+///   ungetchar()
+/// }
+
+/// class Scanner {
+///
+/// ---
+///   nextToken():Token
+/// }
+
+/// class SymTable {
+/// }
+
+/// class Token {
+/// }
+
+/// class Information {
+/// }
+
+/// Scanner -l-> FSM
+/// Scanner --> Buffer
+/// Scanner -u-> SymTable
+/// Scanner -r-> Token
+/// Token --> Information
+
+///@enduml
+
+
+
+

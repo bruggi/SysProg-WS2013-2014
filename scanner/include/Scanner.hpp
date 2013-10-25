180b86fda275c6326df427dac8d1a7b0bdf8533b
JohannesKolb <johannes.kolb88@googlemail.com>
2013-10-25 12:20:03 +0200

    added some little ideas.

---
+++ scanner/include/Scanner.hpp
@@ -0,0 +1,26 @@
+/*
+ * Scanner.hpp
+ *
+ *  Created on: 25.10.2013
+ *      Author: typical
+ */
+
+#ifndef SCANNER_HPP_INCLUDED
+#define SCANNER_HPP_INCLUDED
+
+
+class Scanner {
+
+private:
+
+
+
+public:
+
+
+
+}; // class Scanner
+
+
+
+#endif /* SCANNER_HPP_ */

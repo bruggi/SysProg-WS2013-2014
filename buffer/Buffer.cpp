bd16ae2fad2617df9e822e09f065e1f4e0e4a4e2
Esra Asal <asalesra@outlook.de>
2013-11-06 11:19:40 +0100

    Added buffer to project

---
+++ buffer/Buffer.cpp
@@ -0,0 +1,18 @@
+/*
+ * Buffer.cpp
+ *
+ *  Created on: 11.10.2013
+ *      Author: wei-nad
+ */
+
+#include "Buffer.h"
+
+Buffer::Buffer() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Buffer::~Buffer() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ buffer/Buffer.h
@@ -0,0 +1,17 @@
+/*
+ * Buffer.h
+ *
+ *  Created on: 11.10.2013
+ *      Author: wei-nad
+ */
+
+#ifndef BUFFER_H_
+#define BUFFER_H_
+
+class Buffer {
+public:
+	Buffer();
+	virtual ~Buffer();
+};
+
+#endif /* BUFFER_H_ */

c72eb383be5c6e526ffeda11889c89952d3ecd79
JohannesKolb <johannes.kolb88@googlemail.com>
2014-01-01 22:07:43 +0100

    added more hard-keywords to the symtable at the beginning.

--- symtable/Symtable.cpp
+++ symtable/Symtable.cpp
@@ -9,10 +9,30 @@
 
 Symtable::Symtable() {
 	table = new Hashtable<tokentype::type_t>(50000000);
-	this->insert("if",tokentype::KEY_IF);
-	this->insert("IF",tokentype::KEY_IF);
-	this->insert("while",tokentype::KEY_WHILE);
-	this->insert("WHILE",tokentype::KEY_WHILE);
+	Pair<tokentype::type_t>* currentPair;
+	/*	if	*/
+	currentPair = this->insert("if",tokentype::KEY_IF);
+	currentPair = this->insert("IF",tokentype::KEY_IF);
+
+	/*	while	*/
+	currentPair = this->insert("while",tokentype::KEY_WHILE);
+	currentPair = this->insert("WHILE",tokentype::KEY_WHILE);
+
+	/*	print	*/
+	currentPair = this->insert("print", tokentype::KEY_PRINT);
+	currentPair = this->insert("PRINT", tokentype::KEY_PRINT);
+
+	/*	read	*/
+	currentPair = this->insert("read", tokentype::KEY_READ);
+	currentPair = this->insert("READ", tokentype::KEY_READ);
+
+	/*	else	*/
+	currentPair = this->insert("else", tokentype::KEY_ELSE);
+	currentPair = this->insert("ELSE", tokentype::KEY_ELSE);
+
+	/*	int	*/
+	currentPair = this->insert("int", tokentype::KEY_INT);
+	currentPair = this->insert("INT", tokentype::KEY_INT);
 
 }
 
